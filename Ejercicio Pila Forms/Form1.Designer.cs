1423f21c1b25298f2cd8449e3906f693a3917c8b
Juan Acevedo <jiac_nacho@hotmail.com>
2020-09-28 11:48:00 -0300

    ok

---
+++ Ejercicio Pila Forms/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Ejercicio_Pila_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Carga = new System.Windows.Forms.Label();
+            this.LstCamion = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(357, 191);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(209, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(409, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Carga
+            // 
+            this.Carga.AutoSize = true;
+            this.Carga.Location = new System.Drawing.Point(357, 142);
+            this.Carga.Name = "Carga";
+            this.Carga.Size = new System.Drawing.Size(46, 17);
+            this.Carga.TabIndex = 4;
+            this.Carga.Text = "Carga";
+            // 
+            // LstCamion
+            // 
+            this.LstCamion.FormattingEnabled = true;
+            this.LstCamion.ItemHeight = 16;
+            this.LstCamion.Location = new System.Drawing.Point(602, 142);
+            this.LstCamion.Name = "LstCamion";
+            this.LstCamion.Size = new System.Drawing.Size(272, 164);
+            this.LstCamion.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(622, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Orden de los elementos del camion";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::Ejercicio_Pila_Forms.Properties.Resources.Camion;
+            this.pictureBox1.InitialImage = global::Ejercicio_Pila_Forms.Properties.Resources.Camion;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Image = global::Ejercicio_Pila_Forms.Properties.Resources.Carga;
+            this.button2.Location = new System.Drawing.Point(477, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 76);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Ejercicio_Pila_Forms.Properties.Resources.Descarga;
+            this.button1.Location = new System.Drawing.Point(360, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 74);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(377, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Cargar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Descargar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 448);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LstCamion);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Carga);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Carga;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox LstCamion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
